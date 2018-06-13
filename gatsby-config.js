e796613857817332880300c44449100d01555318
Michael Abdulai <mabdulai90@gmail.com>
2018-06-13 22:52:23 +0200

    Basic settings are all set up.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,49 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Mabdulai gatsby starter'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
-}
+  plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/posts`,
+        name: 'pages'
+      }
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: []
+      }
+    },
+    {
+      // Add your favourite google fonts to your project.
+      // `Roboto\:400,500,700,900`,
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: []
+      }
+    },
+    {
+      // A plugin for generating all essential favicons
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: './src/favicon.png',
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    }
+  ]
+};
