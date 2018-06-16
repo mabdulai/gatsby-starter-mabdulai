584b348f93b775ca6298391fdf42afb867e8fcc7
Michael Abdulai <mabdulai90@gmail.com>
2018-06-16 15:00:44 +0200

    example post

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 
 const IndexPage = () => (
   <div>
@@ -7,7 +7,8 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
+    <Link to="blog/hello-world">Post</Link>
   </div>
-)
+);
 
-export default IndexPage
+export default IndexPage;
